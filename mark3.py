9038b80db0debfe256c70bbe13c3aa8a133b6ebb
papayapeter <t.babel@gmx.com>
2021-05-04 10:51:31 +0200

    added new environment and added colors

--- mark3.py
+++ mark3.py
@@ -2,12 +2,16 @@
 Chatbot Mark based on GPT-3
 """
 import os
+from colorama import init, Fore
 from dotenv import load_dotenv
 
 from modules.chatbot3 import Chatbot3
 
 # load all environment variables from .env files
 load_dotenv()
+
+# initialize colorama on windows
+init()
 
 # load dataset
 with open("fisher_dataset.txt") as file:
@@ -22,4 +26,4 @@ while True:
     if question.lower() == "quit":
         break
 
-    print(mark.ask(question, stop="\n\n"))
+    print(Fore.RED + mark.ask(question, stop="\n\n") + Fore.RESET)
